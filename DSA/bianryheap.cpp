1c47b58f599c2c2b37db96b38a6b265df9dd788c
VIPer05-png <viperflg05@gmail.com>
2026-03-31 15:30:55 +0530

    daily

--- DSA/bianryheap.cpp
+++ DSA/bianryheap.cpp
@@ -1,110 +1,94 @@
-class MinHeap
-{
-    
-    constructor()
-    {
-        this.arr = [];
+#include <iostream>
+using namespace std;
+
+#define MAX 100
+
+int heap[MAX];
+int size = 0;
+
+// Swap function
+void swap(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+// Insert into heap
+void insert(int value) {
+    if (size == MAX) {
+        cout << "Heap is full\n";
+        return;
     }
 
-    left(i) {
-        return 2*i + 1;
+    // Insert at end
+    int i = size;
+    heap[i] = value;
+    size++;
+
+    // Heapify up
+    while (i > 0 && heap[(i - 1) / 2] < heap[i]) {
+        swap(heap[i], heap[(i - 1) / 2]);
+        i = (i - 1) / 2;
+    }
+}
+
+// Delete root
+void deleteRoot() {
+    if (size <= 0) {
+        cout << "Heap is empty\n";
+        return;
     }
 
-    right(i) {
-        return 2*i + 2;
-    }
+    // Replace root with last element
+    heap[0] = heap[size - 1];
+    size--;
 
-    parent(i){
-        return Math.floor((i - 1)/2)
-    }
-    
-    getMin()
-    {
-        return this.arr[0]
-    }
-    
-    insert(k)
-    {
-        let arr = this.arr;
-        arr.push(k);
-    
-        let i = arr.length - 1;
-        while (i > 0 && arr[this.parent(i)] > arr[i])
-        {
-            let p = this.parent(i);
-            [arr[i], arr[p]] = [arr[p], arr[i]];
-            i = p;
-        }
-    }
+    // Heapify down
+    int i = 0;
 
+    while (true) {
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        int largest = i;
 
-    decreaseKey(i, new_val)
-    {
-        let arr = this.arr;
-        arr[i] = new_val;
-        
-        while (i !== 0 && arr[this.parent(i)] > arr[i])
-        {
-           let p = this.parent(i);
-           [arr[i], arr[p]] = [arr[p], arr[i]];
-           i = p;
-        }
-    }
+        if (left < size && heap[left] > heap[largest])
+            largest = left;
 
-    extractMin()
-    {
-        let arr = this.arr;
-        if (arr.length == 1) {
-            return arr.pop();
-        }
-        
-        let res = arr[0];
-        arr[0] = arr[arr.length-1];
-        arr.pop();
-        this.MinHeapify(0);
-        return res;
-    }
+        if (right < size && heap[right] > heap[largest])
+            largest = right;
 
-
-    deleteKey(i)
-    {
-        this.decreaseKey(i, this.arr[0] - 1);
-        this.extractMin();
-    }
-
-    MinHeapify(i)
-    {
-        let arr = this.arr;
-        let n = arr.length;
-        if (n === 1) {
-            return;
-        }
-        let l = this.left(i);
-        let r = this.right(i);
-        let smallest = i;
-        if (l < n && arr[l] < arr[i])
-            smallest = l;
-        if (r < n && arr[r] < arr[smallest])
-            smallest = r;
-        if (smallest !== i)
-        {
-            [arr[i], arr[smallest]] = [arr[smallest], arr[i]]
-            this.MinHeapify(smallest);
+        if (largest != i) {
+            swap(heap[i], heap[largest]);
+            i = largest;
+        } else {
+            break;
         }
     }
 }
 
-let h = new MinHeap();
-    h.insert(3); 
-    h.insert(2);
-    h.deleteKey(1);
-    h.insert(15);
-    h.insert(5);
-    h.insert(4);
-    h.insert(45);
-    
-    console.log(h.extractMin() + " ");
-    console.log(h.getMin() + " ");
-    
-    h.decreaseKey(2, 1); 
-    console.log(h.extractMin());
+// Display heap
+void display() {
+    for (int i = 0; i < size; i++) {
+        cout << heap[i] << " ";
+    }
+    cout << endl;
+}
+
+// Main
+int main() {
+    insert(40);
+    insert(30);
+    insert(35);
+    insert(10);
+    insert(50);
+
+    cout << "Heap after insertion:\n";
+    display();
+
+    deleteRoot();
+
+    cout << "Heap after deletion:\n";
+    display();
+
+    return 0;
+}
