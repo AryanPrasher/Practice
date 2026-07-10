a117048a73cdadf4e7e6b1e42176446c8ac96438
VIPer05-png <viperflg05@gmail.com>
2026-07-10 14:53:42 +0530

    Practice

--- Web Development/JavaScript/todo.js
+++ Web Development/JavaScript/todo.js
@@ -1,5 +1,28 @@
-const { createElement } = require("react")
+function addtodo() {
+    const list = document.getElementById("todo").value;
 
-const list = String(document.getElementById("todo").value)
+    if (list.trim() === "") {
+        return;
+    }
 
-const li = createElement("l1")
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+        <span>${list}</span>
+        <button onclick = "updatetodo(this)">Update</button>
+        <button onclick = "deletetodo(this)">Delete</button>
+    `;
+
+    document.getElementById("todolist").appendChild(li);
+
+    document.getElementById("todo").value = "";
+}
+
+function deletetodo(btn){
+    btn.parentElement.remove()
+}
+
+function updatetodo(btn){
+    const newlist = prompt ("New value")
+    btn.parentElement.querySelector("span").innerHTML=newlist
+}
